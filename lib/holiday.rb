722a7024638e3e58be8c3745465501d6fc710cbe
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-22 18:26:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,10 +63,10 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       if supplies.include?("BBQ")
-        holiday.flatten.compact
+        holiday
       end
     end
-  end
+  end.flatten.compact
 end
 
 
