d56aeb9159747ee6115e484edf35784b43e5ab0f
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-03-22 18:24:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,13 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday, supplies|
+      if supplies.include("BBQ")
+        holiday
+      end
+    end
+  end
 end
 
 
